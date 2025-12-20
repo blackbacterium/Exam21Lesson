75e85ae65a5162394a9e2cce0263c405a692987f
Roman <pimkin-roma@mail.ru>
2025-12-20 17:50:07 +0300

    made a local repository

---
+++ Exam21Lesson/Exam21Lesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam21Lesson
+//
+//  Created by Black Bacterium on 20.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
