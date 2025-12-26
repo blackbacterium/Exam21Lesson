448e3df2376b8e46b3594eb94eb05e26ccdad5d5
Roman <pimkin-roma@mail.ru>
2025-12-26 23:44:21 +0300

    add ShadowView, TeaManager, StackView

--- Exam21Lesson/ViewController.swift
+++ Exam21Lesson/ViewController.swift
@@ -8,13 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let textLabel = UILabel()
+    private let stackView = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
         
+        view.addSubview(textLabel)
     }
 
-
+    private func setupLabel() {
+        textLabel.text = ""
+        textLabel.font = .systemFont(ofSize: 30, weight: .bold)
+        textLabel.textColor = .black
+        textLabel.textAlignment = .center
+    }
+    
+    private func setupStackView() {
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 10
+        
+        stackView.addArrangedSubview(textLabel)
+    }
+    
+    private func setupLayout() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
+            stackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.8)
+        ])
+    }
 }
 
